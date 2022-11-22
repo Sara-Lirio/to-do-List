54fba3c795276517ffd1789dbee86d66246c62a3
Sara <saraeliza.lirio@gmail.com>
2022-11-22 16:25:54 -0300

    feat : Add Componente Add Area

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
+import { useState } from 'react';
 import * as C from './App.styles'
+import { Item } from './types/Item'
+import { ListItem } from './components/ListItem'
+import { AddArea } from './components/AddArea'
 
 const App = () => {
+  const [list, setList] = useState<Item[]>([
+    { id: 1, name: 'Comprar pão', done: true },
+    { id:2, name: 'Fazer bolo', done: false},
+  ])
+
   return (
     <C.Container>
       <C.Area>
         <C.Header>Notas</C.Header>
+
+        <AddArea/>
+        {list.map((item,index)=>(
+          <ListItem key={index} item={item}/>
+        ))}
       </C.Area>
     </C.Container>
   );
